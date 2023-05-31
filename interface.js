17c10c61ebdb14208b131965012dc76d52c80ae6
Bowie <128810414+albertssouza@users.noreply.github.com>
2023-05-31 16:21:58 -0300

    JOGO DA VELHA JS
    PROJETO JOGO DA VELHA EM JAVA SCRIPT

---
+++ interface.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+let squares = document.querySelectorAll(".square");
+squares.forEach((square)=>{
+    square.addEventListener('click',handleClick);
+})
+
+
+})
+
+function handleClick(event){
+    
+    let square = event.target;
+    let postion = square.id;
+
+    if(handleMove(postion)){
+        
+        setTimeout(() =>{
+            alert(" o jogo Acabou");
+
+        }, 10);
+            
+    };
+
+    updateSquares();
+
+    }
+    
+    function updateSquares() {
+        
+        let squares = document.querySelectorAll(".square");
+        
+        squares.forEach((square) => {
+            let postion = square.id;
+            let symbol = board[postion];
+
+        if(Symbol != ''){
+         square.innerHTML = `<div class='${symbol}'></div>`
+
+        }
+
+        })
+    }
+
+
+
+
